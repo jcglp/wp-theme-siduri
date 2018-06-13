c6a32fd1d5a4b53dc3222cedf4d83ff63c13cc00
BANNIA\jc.gomezlobo <jc.gomezlobo@bannia.es>
2018-06-13 10:12:40 +0200

    0.0.5 Creamos single page, y separamos la parte de los comentarios (SIN IMPLEMENTAR AUN)

---
+++ comments.php
@@ -0,0 +1,67 @@
+<!---start-comments-section--->
+<div class="comment-section">
+  <div class="grids_of_2">
+    <h2><?php _e('Comentarios', SIDURI_TEXT_DOMAIN); ?></h2>
+    <div class="grid1_of_2">
+      <div class="grid_img">
+        <a href=""><img src="<?php echo SIDURI_THEME_URL.'/images/pic10.jpg'; ?>" alt=""></a>
+      </div>
+      <div class="grid_text">
+        <h4 class="style1 list"><a href="#">Uku Mason</a></h4>
+        <h3 class="style">march 2, 2013 - 12.50 AM</h3>
+        <p class="para top"> All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.</p>
+        <a href="" class="btn1">Click to Reply</a>
+      </div>
+      <div class="clear"></div>
+    </div>
+
+    <div class="grid1_of_2 left">
+      <div class="grid_img">
+      <a href=""><img src="<?php echo SIDURI_THEME_URL.'/images/pic10.jpg'; ?>" alt=""></a>
+      </div>
+      <div class="grid_text">
+      <h4 class="style1 list"><a href="#">Designer First</a></h4>
+      <h3 class="style">march 3, 2013 - 4.00 PM</h3>
+      <p class="para top"> All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.</p>
+      <a href="" class="btn1">Click to Reply</a>
+      </div>
+    <div class="clear"></div>
+
+    </div>
+    <div class="grid1_of_2">
+    <div class="grid_img">
+    <a href=""><img src="<?php echo SIDURI_THEME_URL.'/images/pic12.jpg'; ?>" alt=""></a>
+    </div>
+    <div class="grid_text">
+    <h4 class="style1 list"><a href="#">Ro Kanth</a></h4>
+    <h3 class="style">march 2, 2013 - 12.50 AM</h3>
+    <p class="para top"> All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.</p>
+    <a href="" class="btn1">Click to Reply</a>
+    </div>
+    <div class="clear"></div>
+    </div>
+    <div class="artical-commentbox">
+    <h2>Leave a Comment</h2>
+    <div class="table-form">
+    <form action="#" method="post" name="post_comment">
+    <div>
+    <label>Name<span>*</span></label>
+    <input type="text" value=" ">
+    </div>
+    <div>
+    <label>Email<span>*</span></label>
+    <input type="text" value=" ">
+    </div>
+    <div>
+    <label>Your Comment<span>*</span></label>
+    <textarea> </textarea>
+    </div>
+    </form>
+    <input type="submit" value="submit">
+
+    </div>
+    <div class="clear"> </div>
+    </div>
+  </div>
+</div>
+<!---//End-comments-section--->
